153bce72e0419514f38068168c70e5507f1024c5
Martin <108198542+mfvc29@users.noreply.github.com>
2025-01-28 09:38:06 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def predecir_precio_y_similares(area_total, dormitorios, banos, estacionamiento,
     }
 
     # Mapear la zona numérica a su nombre de municipio
-    if zona_num <= 5:
+    if zona_num <= 35:
         zona = zonas_municipios[0]
         municipio = ["Barranco", "San Borja", "Santiago de Surco", "Miraflores", "San Isidro", "La Molina",'Jesús María', 'Pueblo Libre', 'Lince', 'San Miguel', 'Magdalena del Mar', 'Surquillo','Cercado de Lima', 'La Victoria', 'Breña', 'Rímac', 'Carabayllo', 'Comas','San Martín de Porres', 'Independencia', 'Los Olivos', 'Ancón', 'Chorrillos','Punta Hermosa', 'San Bartolo', 'Punta Negra', 'Cerro Azul', 'Ate Vitarte','Chaclacayo', 'Chosica', 'San Luis', 'El Agustino', 'Cieneguilla', 'La Perla','Callao', 'Bellavista'][zona_num]
     else:
