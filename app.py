bd202f5a1f2c2271cab0a00d1845e9d65a37191a
Martin <108198542+mfvc29@users.noreply.github.com>
2025-02-28 10:55:45 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ st.set_page_config(page_title="ACM - RE/MAX IRON", page_icon="🏡", layout="wid
 # Título
 col1, col2 = st.columns([1, 4])
 with col1:
-    st.image("Fondo.jpeg", width=80)
+    st.image("Fondo.jpeg", width=120)
 with col2:
     st.title("🏡 Análisis Comparativo de Mercado (ACM)")
 
